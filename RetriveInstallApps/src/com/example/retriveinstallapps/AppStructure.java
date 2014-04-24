00a901ec3e5527af0834776dbd6221045d1e147b
iamvijayakumar <iamvijayakumar@gmail.com>
2014-04-24 17:57:34 +0530

    instaleldapplist

---
+++ RetriveInstallApps/src/com/example/retriveinstallapps/AppStructure.java
@@ -0,0 +1,10 @@
+package com.example.retriveinstallapps;
+
+import android.graphics.drawable.Drawable;
+
+public class AppStructure {
+
+	
+	public String appName;
+	public Drawable appIcons;
+}
---
+++ RetriveInstallApps/src/com/example/retriveinstallapps/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.retriveinstallapps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+	ArrayList<AppStructure> appStructurelist = new ArrayList<AppStructure>();
+
+	AppStructure appStructure;
+	GridView gridView = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.myactivity);
+		gridView = (GridView) findViewById(R.id.gridView1);
+		List<PackageInfo> pakageInstall = getPackageManager()
+				.getInstalledPackages(0);
+		for (int i = 0; i < pakageInstall.size(); i++) {
+			PackageInfo packInfo = pakageInstall.get(i);
+			appStructure = new AppStructure();
+			appStructure.appName = packInfo.applicationInfo.loadLabel(getPackageManager()).toString();
+			appStructure.appIcons = packInfo.applicationInfo.loadIcon(getPackageManager());
+			appStructurelist.add(appStructure);
+			Log.e("error", "" + appStructure.appName);
+			InstalledAppAdapter adapter = new InstalledAppAdapter(this,
+					appStructurelist);
+
+			gridView.setAdapter(adapter);
+
+		}
+	}
+}
