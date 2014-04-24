065d58e7d7dedd4af2da7c5019666ce52cdb61f8
iamvijayakumar <iamvijayakumar@gmail.com>
2014-04-24 17:59:40 +0530

    removed unwanted import

--- RetriveInstallApps/src/com/example/retriveinstallapps/MainActivity.java
+++ RetriveInstallApps/src/com/example/retriveinstallapps/MainActivity.java
@@ -3,21 +3,14 @@ package com.example.retriveinstallapps;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.app.Activity;
 import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
+
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+
 import android.widget.GridView;
-import android.os.Build;
+
 
 public class MainActivity extends Activity {
 
